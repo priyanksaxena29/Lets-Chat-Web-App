d5002f0f9f18093f83461121c5ce2709b5dc45e5
Priyank Saxena <67237826+priyanksaxena29@users.noreply.github.com>
2021-07-01 23:42:30 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,10 +11,36 @@
   // Initialize Firebase  
   firebase.initializeApp(firebaseConfig);
 
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+        console.log("Romm Name - " + Room_names);
+        row  = "<div class = 'room_name' id = " + Room_names + " onclick = 'redirectToRoomName (this.id)' >#" + Room_names +"</div><hr>";
+        document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+
 function addRoom()
 {
  room_name = document.getElementById("room_name").value;
+ 
  firebase.database().ref("/").child(room_name) .update({
      purpose : "adding room"
  })
+
+ localStorage.setItem("room_name" , room_name);
+
+ window.location = "kwitter_page.html";
 }
+
+function redirectToRoomName (name)
+{
+      console.log(name);
+      localStorage.setItem("room_name" , name);
+       window.location = "kwitter_page.html"
+}
+
+names = localStorage.getItem("user_name");
+document.getElementById("input").innerHTML = names;
+console.log(names)
